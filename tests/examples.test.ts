10597a3d2fda1b0fb94869ce40e8fe672cc8f024
RodrigoDornelles <rodrigo@dornelles.me>
2021-02-01 12:55:47 -0300

    implement tests of some examples

--- tests/examples.test.ts
+++ tests/examples.test.ts
@@ -1,9 +1,90 @@
-import { exec, OutputMode } from "https://deno.land/x/exec/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.85.0/testing/asserts.ts";
 
-Deno.test("First test (NO API)", async () => {
-    let cmd = await exec('./3bc.test.bin ./examples/heloworld.3bc',{
-        output: OutputMode.Capture
+/**
+ * @file examples/helloworld.3bc
+ */
+Deno.test("Test Hello world!", async () => {
+    const cmd = await Deno.run({
+        cmd: ["./3bc.test.bin", "./examples/heloworld.3bc"],
+        stdout: "piped",
     });
-      
-   console.log(cmd);
+
+    const output = new TextDecoder().decode(await cmd.output());
+    const { code } = await cmd.status();
+
+    assertEquals(output, "hello world!", "output");
+    assertEquals(code, 0, "return code");
+
+    await cmd.close();
+});
+
+/**
+ * @file examples/loop.3bc
+ */
+Deno.test("Test Looping", async () => {
+    const cmd = await Deno.run({
+        cmd: ["./3bc.test.bin", "./examples/loop.3bc"],
+        stdout: "piped",
+    });
+
+    const output = new TextDecoder().decode(await cmd.output());
+    const { code } = await cmd.status();
+
+    assertEquals(output, "HI!\nHI!\nHI!\n", "output");
+    assertEquals(code, 0, "return code");
+
+    await cmd.close();
+});
+
+/**
+ * @file examples/loop.3bc
+ */
+Deno.test("Test Invert Default", async () => {
+    const cmd = await Deno.run({
+        cmd: ["./3bc.test.bin", "./examples/invert_default.3bc"],
+        stdout: "piped",
+    });
+
+    const output = new TextDecoder().decode(await cmd.output());
+    const { code } = await cmd.status();
+
+    assertEquals(output, "ABC CBA\n", "output");
+    assertEquals(code, 0, "return code");
+
+    await cmd.close();
+});
+
+/**
+ * @file examples/loop.3bc
+ */
+Deno.test("Test Invert Helper", async () => {
+    const cmd = await Deno.run({
+        cmd: ["./3bc.test.bin", "./examples/invert_helper.3bc"],
+        stdout: "piped",
+    });
+
+    const output = new TextDecoder().decode(await cmd.output());
+    const { code } = await cmd.status();
+
+    assertEquals(output, "ABC CBA\n", "output");
+    assertEquals(code, 0, "return code");
+
+    await cmd.close();
+});
+
+/**
+ * @file examples/math_basics.3bc
+ */
+Deno.test("Test Math Basics", async () => {
+    const cmd = await Deno.run({
+        cmd: ["./3bc.test.bin", "./examples/math_basics.3bc"],
+        stdout: "piped",
+    });
+
+    const output = new TextDecoder().decode(await cmd.output());
+    const { code } = await cmd.status();
+
+    assertEquals(code, 0, "return code");
+
+    await cmd.close();
 });
