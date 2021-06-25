65ff1857337701ca3756cfdfe4b2831a1c65cc5d
Rodrigo Dornelles <rodrigo@dynamika.com>
2021-06-25 12:46:18 -0300

    issue #136

--- src/lang_driver.c
+++ src/lang_driver.c
@@ -303,12 +303,12 @@ bool lang_driver_strtol(const char* string, signed long int* value)
     /** custom base with sign **/
     if(decode[0] == '-' && decode[1] == '0' && !isdigit(2) && decode[2] != '\0'){
         type = tolower(decode[2]);
-        memmove(&decode[2], &decode[3], strlen(decode) - 2);
+        memmove(&decode[2], &decode[3], sizeof(decode) - 2);
     }
     /** custom base whiout sign **/
     else if(decode[0] == '0' && !isdigit(1) && decode[1] != '\0') {
         type = tolower(decode[1]);
-        memmove(&decode[1], &decode[2], strlen(decode) - 1);
+        memmove(&decode[1], &decode[2], sizeof(decode) - 1);
     }
     /** decimal base **/
     else {
