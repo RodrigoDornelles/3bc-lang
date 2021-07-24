aaf0954e77930b3fb000eb53dd7188f3246a04bc
RodrigoDornelles <rodrigo@dornelles.me>
2021-07-24 17:57:16 -0300

    add more tests do gpios memory config

--- tests/cpu.test.rb
+++ tests/cpu.test.rb
@@ -40,31 +40,33 @@ class TestCpu < Minitest::Test
 
     def test_mode_6
         console_input = [
-            "mode 0 6\nmmin 1 0\nmmax 1 1\naloc 1 2\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 8\nmmax 1 9\nmcfg 1 0b1110\naloc 1 0\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 8\nmmax 1 9\nmcfg 1 0b1110\naloc 1 2\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 8\nmmax 1 9\nmcfg 1 0b1110\naloc 1 4\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 8\nmmax 1 9\nmcfg 1 0b1110\naloc 1 6\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 8\nmmax 1 9\nmcfg 1 0b1110\naloc 1 8\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 8\nmmax 1 9\nmcfg 1 0b1110\naloc 1 10\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 1\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 2\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 3\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 4\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 5\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 10\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 11\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 12\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 13\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 14\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 19\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 20\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 21\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 22\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0",
-            "mode 0 6\nmmin 1 11\nmmax 1 13\nmcfg 1 0b1110\naloc 1 23\nmode 0 2\nstri 1 0\nmode 0 6\nfree 1 0"
+            "mode.0.6,mmin.1.0,mmax.1.1,aloc.1.2,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.8,mmax.1.9,mcfg.1.0b1110,aloc.1.0,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.8,mmax.1.9,mcfg.1.0b1110,aloc.1.2,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.8,mmax.1.9,mcfg.1.0b1110,aloc.1.4,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.8,mmax.1.9,mcfg.1.0b1110,aloc.1.6,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.8,mmax.1.9,mcfg.1.0b1110,aloc.1.8,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.8,mmax.1.9,mcfg.1.0b1110,aloc.1.10,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.1,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.2,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.3,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.4,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.5,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.10,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.11,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.12,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.13,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.14,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.19,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.20,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.21,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.22,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,mmin.1.11,mmax.1.13,mcfg.1.0b1110,aloc.1.23,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,muse.1.16,muse.1.128,aloc.1.0xFF,mode.0.2,stri.1.0,mode.0.6,free.1.0",
+            "mode.0.6,muse.1.16,aloc.1.0xFF,mode.0.2,stri.1.0,mode.0.6,free.1.0",
         ]
         stdout, stderr, status = Open3.capture3("./3bc.test.bin", :stdin_data => console_input.join("\n"))
-        assert_equal "1888888131112131113111213111311121311", stdout
+        assert_equal "18888881311121311131112131113111213112551", stdout
         assert_equal 0, status
     end
 
