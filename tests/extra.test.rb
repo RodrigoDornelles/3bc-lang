64f9a5981711247f58a49daf70339ada33e36bb0
RodrigoDornelles <rodrigo@dornelles.me>
2021-10-05 20:42:28 -0300

    add extra tests for labels

---
+++ tests/extra.test.rb
@@ -0,0 +1,6 @@
+def test_extra_labels
+    stdout, stderr, status = Open3.capture3("./3bc.test.bin", :stdin_data => "mode.0.9,goto.0.:entry,mode.0.2,0.0.1,strc.0.'e',0.0.2,strc.0.'d',0.0.129,strc.0.'c',0.0.128,strc.0.'b' ,0.0.127,strc.0.'a',strc.0.'.',mode.0.41,back.0.0,mode.0.42,0.0.:entry,call.0.127,call.0.128,call.0.129,call.0.2,call.0.1")
+    assert_equal "", stderr
+    assert_equal "a.ba.cba.dcba.edcba.", stdout
+    assert_equal 0, status
+end
