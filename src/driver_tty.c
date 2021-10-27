775c3ee018ab2c771e8ae3f161b083273bf6e370
RodrigoDornelles <rodrigo@dornelles.me>
2021-10-26 22:15:56 -0300

    test input clamp?

--- src/driver_tty.c
+++ src/driver_tty.c
@@ -95,10 +95,10 @@ data_3bc_t driver_tty_input(register_3bc_t type, address_3bc_t addres)
         }
 
         /** validade input inner memory clamp limits **/
-        if ((driver_memory_data_get(addres) & MEM_CONFIG_MIN_VALUE) == MEM_CONFIG_MIN_VALUE) {
+        if (BITFIELD_HAS(driver_memory_conf_get(addres), MEM_CONFIG_MIN_VALUE)) {
             invalid |= driver_memory_vmin_get(addres) > value;
         }
-        if ((driver_memory_data_get(addres) & MEM_CONFIG_MAX_VALUE) == MEM_CONFIG_MAX_VALUE) {
+        if (BITFIELD_HAS(driver_memory_conf_get(addres), MEM_CONFIG_MAX_VALUE)) {
             invalid |= driver_memory_vmax_get(addres) < value;
         }
     
--- tests/extra.test.rb
+++ tests/extra.test.rb
@@ -3,14 +3,21 @@ require 'minitest/spec'
 require 'minitest/autorun'
 
 class TestFails < Minitest::Test
-    def test_extra_scapes
+    def test_clamp_input
+        stdout, stderr, status = Open3.capture3("./3bc.test.bin", :stdin_data => "mode.0.6,mmin.1.2,mmax.2.1,mode.0.4,stri.1.0,12,stri.2.0,21,mode.0.2,stri.1.0,stri.2.0")
+        assert_equal "", stderr
+        assert_equal "21", stdout
+        assert_equal 0, status
+    end
+
+    def test_scapes
         stdout, stderr, status = Open3.capture3("./3bc.test.bin", :stdin_data => "mode.0.10,4.0.0,mode.0.2,strc.0.'\\0',strc.0.'\\a',strc.0.'\\b',strc.0.'\\t',strc.0.'\\n',strc.0.'\\'',strc.0.'\\\\'")
         assert_equal "", stderr
         assert_equal "000708090A275C", stdout
         assert_equal 0, status
     end
 
-    def test_extra_labels
+    def test_labels
         stdout, stderr, status = Open3.capture3("./3bc.test.bin", :stdin_data => "mode.0.9,goto.0.:entry,mode.0.2,0.0.1,strc.0.'e',0.0.2,strc.0.'d',0.0.129,strc.0.'c',0.0.128,strc.0.'b' ,0.0.127,strc.0.'a',strc.0.'.',mode.0.41,back.0.0,mode.0.42,0.0.:entry,call.0.127,call.0.128,call.0.129,call.0.2,call.0.1")
         assert_equal "", stderr
         assert_equal "a.ba.cba.dcba.edcba.", stdout
