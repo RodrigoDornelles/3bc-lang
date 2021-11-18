f1ab4f61ece1e8d67b1b5719e23f0c9e04b06d3e
Rodrigo Dornelles <rodrigo@dornelles.me>
2021-11-18 12:34:14 -0300

    hot patch ui ui :)

--- src/driver_power.c
+++ src/driver_power.c
@@ -12,6 +12,20 @@ app_3bc_t driver_power_init()
 {
     app_3bc_t app = ds_container_darray_new();
 
+    #if defined(_3BC_COMPUTER)
+    /** TODO: move to driver_tty_init() **/
+    app->tty_source.type = STREAM_TYPE_COMPUTER_STD;
+    app->tty_source.io.file = stdin;
+    app->tty_debug.type = STREAM_TYPE_COMPUTER_STD;
+    app->tty_debug.io.stream = stderr;
+    app->tty_output.type = STREAM_TYPE_COMPUTER_STD;
+    app->tty_output.io.stream = stdout;
+    app->tty_keylog.type = STREAM_TYPE_CLONE_TTY;
+    app->tty_keylog.io.tty = &app->tty_output;
+    app->tty_error.type = STREAM_TYPE_COMPUTER_STD;
+    app->tty_error.io.stream = stderr;
+    #endif
+
     driver_tty_init();
 
     #if defined(_3BC_COMPUTER)
--- src/ds_container_darray.c
+++ src/ds_container_darray.c
@@ -10,6 +10,11 @@ app_3bc_t ds_container_darray_new()
 {
     _instance = (app_3bc_t) malloc(sizeof(struct app_3bc_s));
     _instance->id = 1;
+    /** TODO: move somewhere **/ 
+    _instance->memory.data_get = &ds_memory_llrbt_data_get;
+    _instance->memory.conf_get = &ds_memory_llrbt_conf_get;
+    _instance->memory.data_set = &ds_memory_llrbt_data_set;
+    _instance->memory.conf_set = &ds_memory_llrbt_conf_set;
     return _instance;
 }
 
