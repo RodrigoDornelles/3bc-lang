7072346f1caebe4f0e75997f8b4ca6ccf9abfb41
RodrigoDornelles <rodrigo@dornelles.me>
2021-12-25 09:43:01 -0300

    Update version to 0.1.1 (#247)
    * Update main_plataforms.yml
    
    * Update package.json
    
    * Update library.properties
    
    * Update 3bc.h

--- src/3bc.h
+++ src/3bc.h
@@ -2,10 +2,10 @@
 #define _LANG_3BC_H
 
 /** library info **/
-#define VERSION_3BC "0.1.0"
+#define VERSION_3BC "0.1.1"
 #define VERSION_3BC_MAJOR 0
 #define VERSION_3BC_MINOR 1
-#define VERSION_3BC_PATCH 0
+#define VERSION_3BC_PATCH 1
 
 /** prepare **/
 #include "3bc_macros.h"
