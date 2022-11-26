c0f112052322e473e4cb25f30f596bd94ecd1b54
RodrigoDornelles <rodrigo@dornelles.me>
2022-11-26 10:59:05 -0300

    chore: nes H

--- programs/interpreter_6502.c
+++ programs/interpreter_6502.c
@@ -1,58 +1,42 @@
-/*
-** Fancy hello world program using cc65.
-**
-** Ullrich von Bassewitz (ullrich@von-bassewitz.de)
-**
-*/
 #define _3BC_DISABLE_INTERPRETER
 #define _3BC_ENABLE_CUSTOM
 #include "../src/3bc.h"
+#include "../src/sys/sys_common_pfa.h"
+#include "../src/sys/sys_common_pfa.c"
 
-void str_int()
-{
-    /* Set screen colors */
-    textcolor (COLOR_WHITE);
-    bgcolor (COLOR_BLUE);
-
-    /* Clear the screen, put cursor in upper left corner */
-    clrscr ();
-
-    /* Reset position */
-    gotoxy (0, 0);
-    cprintf ("\n");
-}
+static const tbc_u8_t prog[] = {
+    MODE, NILL, TBC_MODE_STRING,
+    STRC, NILL, 'H',
+    STRC, NILL, 'E',
+    STRC, NILL, 'L',
+    STRC, NILL, 'L',
+    STRC, NILL, 'O',
+    STRC, NILL, '!',
+    STRC, NILL, '\n'
+};
 
 void str_put(char* Text)
 {
     cprintf ("%s", Text);
 }
 
+static struct app_3bc_s instance;
+
 void main (void)
 {
-    unsigned char XSize, YSize;  
-    struct app_3bc_s* const VM = lang_3bc_init();
-
-    str_int();
-    waitvsync();
+    unsigned char XSize, YSize, i;
+    struct app_3bc_s *const VM = &instance;
+    sys_common_pfa888_install(VM);
+    VM->cin.tty_source.io.buf = (tbc_u8_t*) prog;
 
     lang_3bc_print(VM, tty_debug, &str_put);
-    lang_3bc_line(VM, MODE, NILL, TBC_MODE_DEBUG);
-    lang_3bc_line(VM, STRC, NILL, 'H');
-    lang_3bc_line(VM, STRC, NILL, 'E');
-    lang_3bc_line(VM, STRC, NILL, 'L');
-    lang_3bc_line(VM, STRC, NILL, 'L');
-    lang_3bc_line(VM, STRC, NILL, 'O');
-    lang_3bc_line(VM, STRC, NILL, 0x20);
-    lang_3bc_line(VM, STRC, NILL, 'W');
-    lang_3bc_line(VM, STRC, NILL, 'O');
-    lang_3bc_line(VM, STRC, NILL, 'R');
-    lang_3bc_line(VM, STRC, NILL, 'L');
-    lang_3bc_line(VM, STRC, NILL, 'D');
-    lang_3bc_line(VM, STRC, NILL, '!');
-    lang_3bc_line(VM, STRC, NILL, '\n');
 
-    while (1) {
+    for (i = 0; i < 8; ++i) {
+        VM->pkg_func.prog.load(VM);
+        instruction_3bc(VM);
+        VM->pkg_func.prog.next(VM);
         waitvsync();
-        lang_3bc_update(VM);
     }
+    
+    while(1) {};
 }
