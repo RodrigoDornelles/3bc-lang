59b891f08caaeed89e60748baf520575b8afeadb
RodrigoDornelles <rodrigo@dornelles.me>
2023-02-20 16:13:02 -0300

    chore: start types_syscalls.h

---
+++ src/types/types_syscalls.h
@@ -0,0 +1,14 @@
+#ifndef H_TYPES_SYSCALL_TBC
+#define H_TYPES_SYSCALL_TBC
+
+enum ___tbc_syscall_e {
+    TBC_SYS_NONE = 0,
+    TBC_SYS_MEM_READ = 1,
+    TBC_SYS_MEM_WRITE = 2,
+    TBC_SYS_IO_WRITE = 3,
+    TBC_SYS_END = 255
+};
+
+typedef enum ___tbc_syscall_e tbc_syscall_et;
+
+#endif
