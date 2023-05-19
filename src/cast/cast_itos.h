e589355472eb40e540b8929e16f44d6e9cbf4e44
RodrigoDornelles <rodrigo@dornelles.me>
2023-05-19 09:47:38 -0300

    chore: minify deps in cast_itos.h

--- src/cast/cast_itos.h
+++ src/cast/cast_itos.h
@@ -1,7 +1,8 @@
 #ifndef H_CAST_ITOS_TBC
 #define H_CAST_ITOS_TBC
 
-#include "3bc_types.h"
+#include "types_primitive.h"
+#include "types_null.h"
 
 tbc_u8_t cast_itos16(char* dest, void *src, tbc_u8_t dn, const tbc_u8_t sc);
 tbc_u8_t cast_itos10(char* dest, void *src, tbc_u8_t dn, const tbc_u8_t sc);
---
+++ src/types/types_null.h
@@ -0,0 +1,8 @@
+#ifndef H_TYPES_NULL_TBC
+#define H_TYPES_NULL_TBC
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+#endif
