fcd2776cca4b6dc23a014adb6b2ed88659e3889f
RodrigoDornelles <rodrigo@dornelles.me>
2023-05-30 19:08:45 -0300

    chore: add program cafe loop

--- src/cpu/cpu_jump.c
+++ src/cpu/cpu_jump.c
@@ -37,43 +37,53 @@
 
 void cpu_jump_goto(PARAMS_DEFINE)
 {
+    /** @todo rewrite 
     REQUIRED_VALUE
     VALIDATE_NOT_ADRESS
     app->program.label_target = value;
+    */
 }
 
 void cpu_jump_fgto(PARAMS_DEFINE)
 {
+    /** @todo rewrite 
     REQUIRED_VALUE
     VALIDATE_NOT_ADRESS
     if (AUX != 0) {
         app->program.label_target = value;
     }
+    */
 }
 
 void cpu_jump_zgto(PARAMS_DEFINE)
 {
+    /** @todo rewrite 
     REQUIRED_VALUE
     VALIDATE_NOT_ADRESS
     if (AUX == 0) {
         app->program.label_target = value;
     }
+    */
 }
 
 void cpu_jump_pgto(PARAMS_DEFINE)
 {
+    /** @todo rewrite 
     REQUIRED_VALUE
     VALIDATE_NOT_ADRESS
     if (0 < AUX) {
         app->program.label_target = value;
     }
+    */
 }
 
 void cpu_jump_ngto(PARAMS_DEFINE)
 {
+    /** @todo rewrite 
     REQUIRED_VALUE
     VALIDATE_NOT_ADRESS
     if (0 > AUX) {
         app->program.label_target = value;
     }
+    */
 }
