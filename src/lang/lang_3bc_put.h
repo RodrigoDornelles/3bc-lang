b200b0f4ca44f6b877e01f0119444c55f9cd7e71
RodrigoDornelles <rodrigo@dornelles.me>
2023-07-07 12:30:47 -0300

    chore: organize lang_3bc_put

---
+++ src/lang/lang_3bc_put.h
@@ -0,0 +1,20 @@
+#ifndef H_LANG_3BC_PUT_TBC
+#define H_LANG_3BC_PUT_TBC
+
+#include "3bc_types.h"
+
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+void lang_3bc_put(tbc_app_st *const self, char key);
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+#endif
