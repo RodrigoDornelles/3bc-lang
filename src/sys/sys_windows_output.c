c988d9e4aac8ac05c8aadd20f658bf7f6aa3c20a
Matheus C. França <matheus-catarino@hotmail.com>
2023-07-19 13:02:19 -0300

    fix: `incompatible pointer types`

--- src/sys/sys_windows_output.c
+++ src/sys/sys_windows_output.c
@@ -1,25 +1,40 @@
+#ifdef __MINGW32__
+#include <windows.h>
+#else
 #include <Windows.h>
+#endif
 #include "sys_windows_output.h"
 
 void sys_windows_output(tbc_app_st *const self)
 {
-    static LPDWORD ruindows;
-    static HANDLE fileptr = GetStdHandle(STD_OUTPUT_HANDLE);
+    static HANDLE fileptr = NULL;
+
+    // Initialize the fileptr if it hasn't been initialized before
+    if (fileptr == NULL) {
+        fileptr = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (fileptr == INVALID_HANDLE_VALUE) {
+            // Handle error if GetStdHandle fails
+            // (you might want to handle the error accordingly)
+            return;
+        }
+    }
 
     if (self->cache_l3.fixbuf.size > 0) {
+        DWORD bytesWritten;
         WriteFile(
             fileptr,
             self->cache_l3.fixbuf.storage,
-            self->cache_l3.fixbuf.size, 
-            &ruindows,
+            self->cache_l3.fixbuf.size,
+            &bytesWritten,
             NULL
         );
     } else {
+        DWORD bytesWritten;
         WriteFile(
             fileptr,
             self->cache_l3.fixbuf.storage,
-            -self->cache_l3.fixbuf.size, 
-            &ruindows,
+            -self->cache_l3.fixbuf.size,
+            &bytesWritten,
             NULL
         );
     }
