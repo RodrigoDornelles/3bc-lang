d98d4012530a03f79a29cb611a66c859216b5465
RodrigoDornelles <rodrigo@dornelles.me>
2023-11-21 19:39:51 -0300

    chore: doc flag allow heap

--- src/i18n/i18n_en-us_help.c
+++ src/i18n/i18n_en-us_help.c
@@ -6,7 +6,7 @@ static const char ___i18n_cli_help_001[] = "\t-e <text> evaluate program\n";
 static const char ___i18n_cli_help_002[] = "\t-l <size> line max length\n";
 static const char ___i18n_cli_help_003[] = "\t-v -V     version (extra)\n";
 static const char ___i18n_cli_help_004[] = "\t-i -I     vm info (after)\n";
-static const char ___i18n_cli_help_005[] = "\n";
+static const char ___i18n_cli_help_005[] = "\t-m        allow heap\n";
 
 static const tbc_info_txt_st ___i18n_cli_help_txts[] = {
     {sizeof(___i18n_cli_help_000), (char *const)___i18n_cli_help_000},
@@ -15,7 +15,7 @@ static const tbc_info_txt_st ___i18n_cli_help_txts[] = {
     {sizeof(___i18n_cli_help_003), (char *const)___i18n_cli_help_003},
     {sizeof(___i18n_cli_help_004), (char *const)___i18n_cli_help_004},
     {sizeof(___i18n_cli_help_005), (char *const)___i18n_cli_help_005},
-    {0, NULL},
+    {1, "\n"},
 };
 
 const tbc_info_txt_st* tbc_i18n_help_arr = ___i18n_cli_help_txts;
