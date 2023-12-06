bb0e88560461b07be91c44c056910f7001d6e469
rodrigodornelles <rodrigo@dornelles.me>
2023-12-06 13:04:58 -0300

    docs: detect_libc.h

--- src/detect/detect_libc.h
+++ src/detect/detect_libc.h
@@ -1,20 +1,60 @@
+/**
+ * @file src/detect/detect_libc.h
+ * @short detect C library
+ * @brief recognize system abi library 
+ */
 #ifndef H_DETECT_LIBC_TBC
 #define H_DETECT_LIBC_TBC
 
-#if defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_AVR)
+/**
+ * @short Arduino SDK
+ * @c <Arduino.h> 
+ * @date 2005
+ * @author Arduino Team
+ * @copyright GNU Lesser General Public License
+ */
+#if defined(DOXYGEN) || defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_AVR)
 #ifndef TBC_LIBC_ARDUINO
 #define TBC_LIBC_ARDUINO
 #endif
 #endif
 
-#if defined(__CC65_STD__) || defined(__CC65_STD_CC65__) || defined(__MSX__)
+/**
+ * @short MS-DOS Library
+ * @c <conio.h> 
+ * @date 1981
+ * @author Microsoft
+ */
+#if defined(DOXYGEN) || defined(__CC65_STD__) || defined(__CC65_STD_CC65__) || defined(__MSX__)
 #ifndef TBC_LIBC_CONIO
 #define TBC_LIBC_CONIO
 #endif
 #endif
 
-#if !defined(TBC_TCC_NOSTDINC)
-#if defined(__linux) || defined(__linux__) || defined(__gnu_linux__) ||\
+/**
+ * @short Tiny C Compiler Library
+ * @c <tcclib.h>
+ * @date 2001
+ * @author Fabrice Bellard
+ * @copyright GNU Lesser General Public License
+ */
+#if defined(DOXYGEN) || defined(TBC_TCC_NOSTDINC)
+#ifndef TBC_LIBC_TCC
+#define TBC_LIBC_TCC
+#endif
+#endif
+
+/**
+ * @short Posix Standard Library
+ * @c <unistd.h>
+ * @c <fcntl.h>
+ * @date 2001
+ * @author Fabrice Bellard
+ * @copyright GNU Lesser General Public License
+ */
+#if defined(DOXYGEN) || !defined(TBC_TCC_NOSTDINC)
+#if defined(DOXYGEN) ||\
+     defined(__linux) || defined(__linux__) || defined(__gnu_linux__) ||\
      defined(__APPLE__) || defined(__unix__) || defined(BSD)
 #ifndef TBC_LIBC_POSIX
 #define TBC_LIBC_POSIX
@@ -22,7 +62,13 @@
 #endif
 #endif
 
-#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(_WIN64)
+/**
+ * @short Windows API Library
+ * @c <Windows.h> 
+ * @date 1985
+ * @author Microsoft
+ */
+#if defined(DOXYGEN) || defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(_WIN64)
 #ifndef TBC_LIBC_WINDOWS
 #define TBC_LIBC_WINDOWS
 #endif
